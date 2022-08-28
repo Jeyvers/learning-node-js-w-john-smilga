a83814467280ff5436b724c41e09b0358fc771a3
Jeyi Adole <erthnice@gmail.com>
2022-08-28 21:48:42 +0100

    Ended for today.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('user hit the server');
+  res.end('home page');
 });
+
+// you always need to add the response.end()
 
 // A port is a communication endpoint, there are specific ports for different things.
 server.listen(5000);
+
+// Stopped at 4.14.22
+// Start at 4.
