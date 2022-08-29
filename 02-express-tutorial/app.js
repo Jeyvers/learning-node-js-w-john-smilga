9e30f3471e547f0933bd0c650e27010d56be11d4
Jeyi Adole <erthnice@gmail.com>
2022-08-29 10:01:00 +0100

    Explaining ports and how the web works with them.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+
+// createServer creates the server and takes in a callback which will be invoked every single time the user hits the server.
 const server = http.createServer((req, res) => {
   console.log('user hit the server');
   res.end('home page');
@@ -6,7 +8,7 @@ const server = http.createServer((req, res) => {
 
 // you always need to add the response.end()
 
-// A port is a communication endpoint, there are specific ports for different things.
+// A port is a number assigned to uniquely identify a connection endpoint and to direct data to a specific service. There are specific ports for different things.
 server.listen(5000);
 
 // Stopped at 4.14.22
