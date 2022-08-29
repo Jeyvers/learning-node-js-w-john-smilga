a1f1ef7f935c38bd5a990d0379781de93771702c
Jeyi Adole <erthnice@gmail.com>
2022-08-29 10:38:45 +0100

    Stopped for now.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,23 @@ const http = require('http');
 
 // createServer creates the server and takes in a callback which will be invoked every single time the user hits the server.
 const server = http.createServer((req, res) => {
-  console.log('user hit the server');
-  res.end('home page');
+  // STATUS_CODES
+  // MIME_TYPES
+  const url = req.url;
+
+  if (url == '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Home page</h1>');
+    res.end();
+  } else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>About page</h1>');
+    res.end();
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1> page not found</h1>');
+    res.end();
+  }
 });
 
 // you always need to add the response.end()
@@ -11,5 +26,4 @@ const server = http.createServer((req, res) => {
 // A port is a number assigned to uniquely identify a connection endpoint and to direct data to a specific service. There are specific ports for different things.
 server.listen(5000);
 
-// Stopped at 4.14.22
-// Start at 4.
+// Stopped at 4.32.09
