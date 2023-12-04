9ee8b420bc43d1b5971de8c651ca236f2890a135
Jeyi Adole <meetjeyvers@gmail.com>
2023-12-04 11:54:12 +0100

    model, view, controllers, clean routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,76 +1,18 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
-
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 // static assets
 app.use(express.static("./methods-public"));
 
 // parse form data with middleware so we can have access to it
 // app.use(express.urlencoded({ extended: false })); // for html form method
 app.use(express.json());
+app.use("/api/people", people);
+app.use("/login", auth);
 
-app.get("/api/people", (req, res) => {
- res.status(200).json({ success: true, data: people });
-});
-
-app.post("/api/people", (req, res) => {
- const { name } = req.body;
- if (!name) {
-  return res
-   .status(400)
-   .json({ success: false, msg: "please provide name value" });
- }
- res.status(201).json({ success: true, person: name });
-});
-
-app.post("/api/postman/people", (req, res) => {
- const { name } = req.body;
- if (!name) {
-  return res
-   .status(400)
-   .json({ success: false, msg: "please provide name value" });
- }
- res.status(201).json({ success: true, data: [...people, { name }] });
-});
-
-app.post("/login", (req, res) => {
- const { name } = req.body;
- if (name) {
-  return res.status(200).send(`Welcome ${name}`);
- }
-
- res.status(401).send("Please provide credentials");
-});
-
-app.put("/api/people/:id", (req, res) => {
- const { id } = req.params;
- const { name } = req.body;
- const person = people.find((person) => person.id === +id);
- if (!person) {
-  return res
-   .status(404)
-   .json({ success: false, msg: `Person ${id} doesn't exist` });
- }
- const newPeople = people.map((person) => {
-  if (person.id === +id) {
-   person.name = name;
-  }
-  return person;
- });
- res.status(200).json({ success: true, data: newPeople });
-});
-
-app.delete("/api/people/:id", (req, res) => {
- const { id } = req.params;
- const person = people.find((person) => person.id === +id);
- if (!person) {
-  return res
-   .status(404)
-   .json({ success: false, msg: `Person ${id} doesn't exist` });
- }
- const newPeople = people.filter((person) => person.id !== +id);
- res.status(200).json({ success: true, data: newPeople });
-});
+// MVC (Model-View-Controller) is a pattern in software design commonly used to implement user interfaces, data, and controlling logic.
+// This is the model
 
 app.listen(4999, (req, res) => {
  console.log("Server listening on port 4999");
